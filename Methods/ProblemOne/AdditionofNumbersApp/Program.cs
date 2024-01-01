606d13f1ed1df60170fa24cd32e18b833ff2ffcb
Sheikh Hamim Mahmud <hamim.mahmud11@gmail.com>
2024-01-02 00:16:04 +0600

    mehods program

---
+++ Methods/ProblemOne/AdditionofNumbersApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionofNumbersApp
+{
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your First Number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Second Number:");
+            int lastNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The sum is:"+ GetAddition(firstNumber, lastNumber));
+            Console.ReadKey();
+        }
+        public static int GetAddition(int a, int b)
+        {
+
+            return a + b;
+        }
+    }
+}
