9d4ccda8f3a342058218ee420a121eb6a0972ce3
Sheikh Hamim Mahmud <hamim.mahmud11@gmail.com>
2024-01-01 23:59:22 +0600

    commit for push

---
+++ Array/ProblemOne/GetRollNumberApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetRollNumberApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your array size:");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] rollNumber = new int[size];
+            Console.WriteLine($"Your array size is: {size}");
+            for (int i = 0; i < rollNumber.Length; i++)
+            {
+                rollNumber[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Your Numbers are:");
+            for(int i = 0; i < rollNumber.Length; i++)
+            {
+                Console.WriteLine(rollNumber[i]);
+            }
+            Console.WriteLine("Task Done!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Array/ProblemTwo/SumOfNnumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfNnumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your last number:");
+            int lastNumber = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[lastNumber];
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Enter Your {i + 1} number:");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int sum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum = sum+numbers[i];
+            }
+            Console.WriteLine("The Sum is: {0} ", sum);
+            Console.ReadKey();
+        }
+    }
+}
